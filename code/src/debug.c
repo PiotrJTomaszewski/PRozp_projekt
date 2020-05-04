bc17517bc22dac27df4da422278cf4b493df2eb1
PiotrJTomaszewski <pjtomaszewski98@gmail.com>
2020-05-05 00:07:35 +0200

    Implement debug printf

--- code/src/debug.c
+++ code/src/debug.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
 
 #include "debug.h"
 #include "tourist.h"
 
-void debug_print(debug_msg_type_t msg_type, int id, state_t state, char *text) {  
+#define NOTIMPLEMENTED(a) ((void)a)
+#define BUFFER_SIZE 256
+
+void debug_print(debug_msg_type_t msg_type, tourist_t *tourist, char *text) {
     // TODO: Implement colors or sth like that
-    char *state_name = stateNames[state];
-    printf("[%d] - %s - %s\n", id, state_name, text);
+    printf("Id %d - Clock %d - %s - %s\n", tourist->id, tourist->lamport_clock, stateNames[tourist->state], text);
+}
+
+void debug_printf(debug_msg_type_t msg_type, tourist_t *tourist, char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    printf("Id %d - Clock %d - %s - ", tourist->id, tourist->lamport_clock, stateNames[tourist->state]);
+    vprintf(format, args);
+    puts(""); // Print new line
+    va_end(args);
 }
