998986c5e6eb7c037ca78d5a01a9c2aaae060cdb
PiotrJTomaszewski <pjtomaszewski98@gmail.com>
2020-06-14 01:27:42 +0200

    Remove old C version

--- code/src/globals.h
+++
@@ -1,4 +0,0 @@
-#ifndef __GLOBALS_H__
-#define __GLOBALS_H__
-
-#endif
--- code/valgrind_run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mpiexec -np 2 valgrind --leak-check=full --show-reachable=yes --log-file=logs/nc.vg.%p ./nanozombie
